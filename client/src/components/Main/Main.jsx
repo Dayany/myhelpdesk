88c6935649051811cb6afe86b69c89eee9fb8447
Dayan Yamin <yamindayan@gmail.com>
2021-10-06 19:58:34 -0400

    Added buttons for each sections

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Grid, Stack, Typography } from "@mui/material";
+import useStyles from "../../styles";
 
 const Main = () => {
+  const classes = useStyles();
   return (
     <>
       <Container maxWidth="lg">
@@ -12,6 +14,29 @@ const Main = () => {
         >
           Help Desk Dashboard
         </Typography>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "50vh" }}
+        >
+          <Stack spacing={2} direction="row" className={classes.spacing}>
+            <Grid item xs={6}>
+              <Button href="/users" variant="contained">
+                Users
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained">Departments</Button>
+            </Grid>
+          </Stack>
+          <Stack spacing={2} direction="row" className={classes.spacing}>
+            <Button variant="contained">Equipments</Button>
+            <Button variant="contained">Jobs/Tasks</Button>
+          </Stack>
+        </Grid>
       </Container>
     </>
   );
