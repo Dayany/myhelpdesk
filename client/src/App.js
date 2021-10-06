d210d0e35d828340eeabbafbae4f963ce918957a
Dayan Yamin <yamindayan@gmail.com>
2021-10-05 20:21:31 -0400

    Setup Main dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import { Toolbar, Typography } from "@mui/material";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <MuiAppBar position="relative" />
+      <Toolbar>
+        <Typography variant="h6">My Help Desk</Typography>
+      </Toolbar>
+      <main>
+        <Main />
+      </main>
+    </>
   );
 }
 
---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import { Container, Typography } from "@mui/material";
+
+const Main = () => {
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Typography
+          variant="h3"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Help Desk Dashboard
+        </Typography>
+      </Container>
+    </>
+  );
+};
+
+export default Main;
