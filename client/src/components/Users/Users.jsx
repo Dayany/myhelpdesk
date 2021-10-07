ee6b367c6c82b2074c714772fe03c5cf2b9444db
Dayan Yamin <yamindayan@gmail.com>
2021-10-06 23:11:05 -0400

    Fetch and show users in a table

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,6 +1,22 @@
 import { Container, Typography } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { useEffect, useState } from "react";
 
 const Users = () => {
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(`${process.env.REACT_APP_API_SERVER}users`);
+      const json = await response.json();
+      setUsers(json);
+    }
+    fetchUsers();
+  }, []);
+
   return (
     <>
       <Container maxWidth="lg">
@@ -12,6 +28,27 @@ const Users = () => {
         >
           Users
         </Typography>
+        <Typography variant="h5">Current Users</Typography>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Department</TableCell>
+              <TableCell>Role</TableCell>
+              <TableCell>Active?</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users?.map((row) => (
+              <TableRow key={row._id}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.department}</TableCell>
+                <TableCell>{row.role}</TableCell>
+                <TableCell>{row.isActive ? "Yes" : "No"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Container>
     </>
   );
