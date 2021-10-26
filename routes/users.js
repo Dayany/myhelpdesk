393a7f1fffc6842279a489113885229d978e8caa
Dayan Yamin <yamindayan@gmail.com>
2021-10-25 20:57:19 -0400

    Provide with route to login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Get all users
 router.get("/", async (req, res) => {
@@ -33,4 +34,27 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Login User
+router.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.findOne({ username }).lean();
+
+  console.log(user);
+  if (!user) {
+    return res.json({
+      status: "error",
+      error: "Invalid User/Password combination",
+    });
+  }
+
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+
+    return res.json({ status: "OK", data: token });
+  }
+
+  res.json({ status: "error", error: "User/Password combination" });
+});
 module.exports = router;
