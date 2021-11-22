b0a66fade9b4bc40d778fee8fb442154174d755d
Dayan Yamin <yamindayan@gmail.com>
2021-11-21 23:09:25 -0400

    Improve JWT structure within files

--- client/src/API.js
+++ client/src/API.js
@@ -2,12 +2,7 @@ import axios from "axios";
 
 const axiosAPI = axios.create({
   baseURL: "/api/",
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
-
-// axiosAPI.interceptors.request.use((req) => {
-//   if (localStorage.getItem("token")) {
-//     req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-//   }
-// });
 
 export default axiosAPI;
